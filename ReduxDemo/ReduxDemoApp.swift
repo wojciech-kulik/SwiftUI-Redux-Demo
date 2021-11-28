43233010d508c7b5a917c337e2f9a83b3bcab801
Wojciech Kulik <wkulik91@gmail.com>
2021-11-28 12:13:01 +0100

    Initial Commit

---
+++ ReduxDemo/ReduxDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReduxDemoApp.swift
+//  ReduxDemo
+//
+//  Created by Wojciech Kulik on 28/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct ReduxDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
